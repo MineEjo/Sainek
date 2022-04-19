84042fbe9bf61267430649c93c2257230ea1ebac
MineEjo <evgenyshaimov@gmail.com>
2022-04-19 22:02:32 +0300

    Added func for get locale

--- src/functions/loadLocale.js
+++ src/functions/loadLocale.js
@@ -4,4 +4,8 @@ if (LANG.has(USER_LANG)) {
 	for (const _hElement of document.getElementsByClassName('locale')) {
 		_hElement.innerHTML = LOCALE[_hElement.innerHTML];
 	}
+
+	function _getLocale(_sKey) {
+		return LOCALE[_sKey]
+	}
 }
