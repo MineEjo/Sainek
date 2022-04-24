5367820f050a0391734535805ed232c078ed0cea
MineEjo <evgenyshaimov@gmail.com>
2022-04-24 12:46:27 +0300

    Added copyright

--- src/functions/elements.js
+++ src/functions/elements.js
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2022 MineEjo.
+ * This file is part of Sainek-Anime-Keeper <https://github.com/MineEjo/Sainek-Anime-Keeper>.
+ *
+ * Sainek-Anime-Keeper is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sainek-Anime-Keeper is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JJkBot.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 function createDiv(_hParent, _sId, _sClass, _fFunction) {
 	const _hChild = document.createElement('div');
 	_hChild.tabIndex = 1;
