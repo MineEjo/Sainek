801560f123a7a2611864dabbd09565276789b83a
MineEjo <evgenyshaimov@gmail.com>
2022-04-30 14:29:31 +0300

    Corrected typos in copyright
    Signed-off-by: MineEjo <evgenyshaimov@gmail.com>

--- src/website/board.js
+++ src/website/board.js
@@ -13,7 +13,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with JJkBot.  If not, see <http://www.gnu.org/licenses/>.
+ * along with  Sainek-Serials-Keeper.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /* Moved out, for changes in other scripts */
